7501597bd2c3b2d6615258c76c02a27557824031
Aiden Fox-Ivey <71291879+Foxnaut@users.noreply.github.com>
2021-07-20 22:03:14 -0400

    Got it to function basic approach.

--- StockBot.py
+++ StockBot.py
@@ -5,31 +5,37 @@ import os
 
 
 def main():
-	# program logic goes here
-	
-	load_dotenv
-	TOKEN = os.getenv('DISCORD_TOKEN')
+	# configure tokens
+	tokens = set_tokens()
 
-def logging():
-	# logging code here
+	# configure logging
+	set_logging()
+
+	intents = discord.Intents.default()
+	intents.messages = True
+
+	bot = StockBot(intents=intents)
+	bot.run(tokens[0])
+
+def set_tokens():
+	# loads dotenv files in the local directory
+	load_dotenv()
+	TOKEN = os.getenv('DISCORD_TOKEN')
+	GUILD = os.getenv('DISCORD_GUILD')
+	return [TOKEN, GUILD]
+
+def set_logging():
 	logger = logging.getLogger('discord')
 	logger.setLevel(logging.DEBUG)
 	handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-	# specifically writes to discord.log
 	handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 	logger.addHandler(handler)
 
 
 
 class StockBot(discord.Client):
-	bot = commands.self(command_prefix='?', description=description, intents=intents) 	
-
-	# all event handlers must be coroutines
-
-	# no need for decorator when in class definition
-
 	async def on_ready(self):
-		print(f"We've logged on as {self.user}")
+		print(f"We've logged on as {self.user}.")
 		# doesn't need to be awaited - non blocking function
 
 	async def on_message(self, message):
@@ -38,13 +44,6 @@ class StockBot(discord.Client):
 		if message.content.startswith('$hello'):
 			await message.channel.send('hello!')
 	
-	async def help_call(message):
+	async def help_call(self, message):
 		if message.content("help"):
 			await message.channel.send('let me give you some help')
-
-
-	
-client = StockBot()
-
-# just for jokes, the token needs to go here in the actual call
-client.run(TOKEN)
